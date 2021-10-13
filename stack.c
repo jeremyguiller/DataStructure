a48f078bed67bcfa2432b524518d55a59fa8aad3
Jeremyguiller <guillerjeremy@gmail.com>
2021-10-13 11:03:45 +0200

    stack.c to test

--- stack.c
+++ stack.c
@@ -5,7 +5,47 @@
  */
 
 #include "stack.h"
-
+void init_stack(Stack *s){
+    s->index = 0;
+    for (int i = 0; i < STACK_MAX_SIZE; ++i) {
+        s->data[i] = 0;
+    }
+}
 void push(Stack *s, float value) {
-    
+    s->index += 1;
+    s->data[s->index] = value;
+}
+
+float pop(Stack *s){
+    float a = s->data[s->index];
+    s->index -= 1;
+    return a;
+}
+
+bool is_stack_empty(Stack *s){
+    if (s->index == 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+float peek(Stack *s){
+    return s->data[s->index];
+}
+void dup(Stack *s){
+    float a = s->data[s->index];
+    s->index += 1;
+    s->data[s->index] = a;
+}
+
+void swap(Stack *s){
+    float a = s->data[s->index];
+    float b = s->data[s->index-1];
+    s->data[s->index] = b;
+    s->data[s->index-1] = a;
+}
+
+void clear(Stack *s){
+    s->index = 0;
 }
