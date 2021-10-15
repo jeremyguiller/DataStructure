cc74ae0c44d15b0e59044e85c0ae83fd4fcb2307
BENUCCI <maxime.benucci@etu.univ-st-etienne.fr>
2021-10-15 09:20:15 +0200

    Queue & array liste a commenté

--- array_list.c
+++ array_list.c
@@ -2,10 +2,19 @@
 #include <stdlib.h>
 #include <Array_list.h>
 
+/* *
+ * @bref Init Array liste
+ */
 void init_array_list(Array_list *l){
     l->index=0;
 }
 
+/* *
+ * @brief Insérer la valeur à la position
+ * @param l : Array qui recevra la valeur
+ * @param position : Position ou inserer
+ * @param value : Valeur à ajouter
+ */
 void insert_at(Array_list *l, int position, float value){
     if (position > l->index){
         add(l, value);
@@ -20,12 +29,21 @@ void insert_at(Array_list *l, int position, float value){
     }
 }
 
-
+/* *
+ * @brief Ajout de la valeur au tableau
+ * @param l : Liste des tableaux
+ * @param value : valeur à ajouter
+ */
 void add(Array_list *l, float value){
     l->data[l->index] = value;
     l->index++;
 }
 
+/* *
+ * @brief Supprimer la valeur à la position
+ * @param l : liste de tableaux
+ * @param position : Supprimer la valeur position
+ */
 float remove_at(Array_list *l, int position){
     float temp = l->data[position];
     for (int i = position; i < l->index; i++){
@@ -35,10 +53,19 @@ float remove_at(Array_list *l, int position){
     return temp;
 }
 
+/* *
+ * @bref Recuperer la valeur
+ * @param l : liste de tableaux
+ * @param position : Index de la valeur à retourner
+ */
 float get_at(Array_list *l,int position){
     return l->data[position];
 }
 
+/* *
+ * @brief Effacer la liste des tableaux
+ * @param l : Liste des tableaux à effacer
+ */
 void clear(Array_list *l){
     init_array_list(l);
 }
--- queue.c
+++ queue.c
@@ -2,10 +2,17 @@
 #include <stdlib.h>
 #include <queue.h>
 
+/* *
+ * @bref Inistialisation init_queue
+ * @param s : à initialiser
+ */
 void init_queue(Queue *s){
     s->index = 0;
 }
 
+/* *
+ * @brief Empiler
+ */
 void enqueue(Queue *q, float value){
     for (int i = q->index; i >= 0; i--){
         q->data[i + 1] = q->data[i];
@@ -14,6 +21,9 @@ void enqueue(Queue *q, float value){
     q->index++;
 }
 
+/* *
+ * @brief Desempiler
+ */
 float dequeue(Queue *q){
     {
         float result = q->data[q->index-1];
@@ -21,14 +31,24 @@ float dequeue(Queue *q){
         return result;
     }
 
+/* *
+ * @brief Vérifie si la file est vide
+ */
 bool is_queue_empty(Queue *q){
     return q->index == 0;
 }
 
+/* *
+ * @bref retourne la premiere valeur de la file
+ */
 float front(Queue *q) { //aka peek
     return q->data[q->index-1];
 }
 
+/* *
+ * @brief Effacer la file
+ * @param q File à effacer
+ */
 void clear(Queue *q){
     init_queue(q);
 }
